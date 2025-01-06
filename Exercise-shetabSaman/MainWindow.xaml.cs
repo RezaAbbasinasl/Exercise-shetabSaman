2163085270c1268532d29b83cb697812e5dd5e46
USER <USER@DESKTOP-2R09BSK>
2025-01-07 01:04:38 +0330

    Add project files.

---
+++ Exercise-shetabSaman/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise_shetabSaman
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenWindows_Click(object sender, RoutedEventArgs e)
+        {
+            var screens = Screen.AllScreens;
+
+            foreach (var screen in screens)
+            {
+                var monitorWindow = new MonitorWindow(screen);
+                monitorWindow.Show();
+            }
+        }
+    }
+}
---
+++ Exercise-shetabSaman/MonitorWindow.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace Exercise_shetabSaman
+{
+    public class MonitorWindow : Window
+    {
+        public MonitorWindow(Screen screen)
+        {
+            this.Width = screen.Bounds.Width / 2;
+            this.Height = screen.Bounds.Height / 2;
+            this.Left = screen.Bounds.Left + (screen.Bounds.Width - this.Width) / 2;
+            this.Top = screen.Bounds.Top + (screen.Bounds.Height - this.Height) / 2;
+
+            var button = new System.Windows.Controls.Button
+            {
+                Content = "Close",
+                Width = 100,
+                Height = 50,
+            };
+            button.Click += (s, e) => this.Close();
+            this.Content = button;
+        }
+    }
+}
